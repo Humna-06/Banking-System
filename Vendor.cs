ff5d00194b42e76658e515770d4e75ff14c96fd3
Humna-06 <humnakhalid438@gmail.com>
2025-05-24 09:31:51 +0500

    I have added all the classes from word document

---
+++ Vendor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_System
+{
+    internal class Vendor
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        public Vendor(string name, string phone, string address)
+        {
+            Name = name;
+            Phone = phone;
+            Address = address;
+        }
+
+    }
+}
